34679f411c7dbc8755a659248b466091ba0dc7b9
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2021-02-04 21:41:43 +0200

    1.0 update
    fixed formatting and spacing

--- schedule.ps1
+++ schedule.ps1
@@ -7,45 +7,42 @@
 .DESCRIPTION Downloads the latest version of nvidia-update and registers a scheduled task. 
 #>
 
-
 ## Constant variables and functions
 New-Variable -Name "rawRepo" -Value "https://raw.githubusercontent.com/ZenitH-AT/nvidia-update/master" -Option Constant
 
 function Write-ExitError {
-    param (
-        [string] $ErrorMessage
-    )
-    
-    Write-Host -ForegroundColor Yellow $ErrorMessage
-    Write-Host "Press any key to exit..."
+	param (
+		[string] $ErrorMessage
+	)
 
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	Write-Host -ForegroundColor Yellow $ErrorMessage
+	Write-Host "Press any key to exit..."
 
-    exit
+	$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+	exit
 }
-
 
 ## Check internet connection
 if (!(Get-NetRoute | Where-Object DestinationPrefix -eq "0.0.0.0/0" | Get-NetIPInterface | Where-Object ConnectionState -eq "Connected")) {
-    Write-ExitError "No internet connection. After resolving connectivity issues, please try running this script again."
+	Write-ExitError "No internet connection. After resolving connectivity issues, please try running this script again."
 }
-
 
 ## Register scheduled task
 $taskDir = "$($env:USERPROFILE)\nvidia-update"
 $taskPath = "$($taskDir)\nvidia-update.ps1"
 
 if (!(Test-Path $taskDir)) {
-    New-Item -Path $taskDir -ItemType "directory" | Out-Null
+	New-Item -Path $taskDir -ItemType "directory" | Out-Null
 }
 
 # Get latest script version from repository
 try {
-    $latestScriptVersion = Invoke-WebRequest -Uri "$($rawRepo)/version.txt"
+	$latestScriptVersion = Invoke-WebRequest -Uri "$($rawRepo)/version.txt"
 	$latestScriptVersion = "$($latestScriptVersion)".Trim()
 }
 catch {
-    Write-ExitError "Unable to determine latest script version. Please try running this script again."
+	Write-ExitError "Unable to determine latest script version. Please try running this script again."
 }
 
 $taskName = "nvidia-update $($latestScriptVersion)"
@@ -63,39 +60,39 @@ $registerTask = $true
 $existingTasks = Get-ScheduledTask | Where-Object { $_.TaskName -match "^nvidia-update." }
 
 foreach ($existingTask in $existingTasks) {
-    $registerTask = $false
+	$registerTask = $false
 
-    if ($existingTask.TaskName -notlike "*$($latestScriptVersion)") {
-        Unregister-ScheduledTask -TaskName $existingTask.TaskName -Confirm:$false
+	if ($existingTask.TaskName -notlike "*$($latestScriptVersion)") {
+		Unregister-ScheduledTask -TaskName $existingTask.TaskName -Confirm:$false
 
-        $registerTask = $true
-    }
+		$registerTask = $true
+	}
 }
 
 if ($registerTask) {
-    # Download lastest script files
-    try {
-        Invoke-WebRequest -Uri "$($rawRepo)/nvidia-update.ps1" -OutFile $taskPath
-        Invoke-WebRequest -Uri "$($rawRepo)/optional-components.cfg" -OutFile "$($taskDir)\optional-components.cfg"
-    }
-    catch {
-        Write-ExitError "Downloading script files failed. Please try running this script again."
-    }
+	# Download lastest script files
+	try {
+		Invoke-WebRequest -Uri "$($rawRepo)/nvidia-update.ps1" -OutFile $taskPath
+		Invoke-WebRequest -Uri "$($rawRepo)/optional-components.cfg" -OutFile "$($taskDir)\optional-components.cfg"
+	}
+	catch {
+		Write-ExitError "Downloading script files failed. Please try running this script again."
+	}
 
-    Write-Host "Downloaded the latest script to `"$($taskPath)`".`n"
+	Write-Host "Downloaded the latest script to `"$($taskPath)`".`n"
 
-    Register-ScheduledTask -TaskName $taskName -Action $action -Settings $settings -Trigger $trigger -Description $description | Out-Null
+	Register-ScheduledTask -TaskName $taskName -Action $action -Settings $settings -Trigger $trigger -Description $description | Out-Null
 
-    Write-Host -ForegroundColor Green "Scheduled task registered."
+	Write-Host -ForegroundColor Green "Scheduled task registered."
 }
 else {
-    Write-Host -ForegroundColor Gray "Scheduled task for the latest script version is already registered."
+	Write-Host -ForegroundColor Gray "Scheduled task for the latest script version is already registered."
 }
 
 $decision = $Host.UI.PromptForChoice("", "`nWould you like to run nvidia-update?", ("&Yes", "&No"), 0)
 
 if ($decision -eq 0) {
-    Start-Process PowerShell $taskPath
+	Start-Process PowerShell $taskPath
 }
 
 Write-Host "`nExiting script in 5 seconds..."
