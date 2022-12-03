4299b367c97e6ce47e7812d9670c419ebf0da10b
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2022-12-03 13:17:26 +0200

    1.8; simplify

--- schedule.ps1
+++ schedule.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.7.1
+.VERSION 1.8
 .GUID 544ddf4b-d7df-44b2-abcf-f452793c0fa7
 .AUTHOR ZenitH-AT
 .LICENSEURI https://raw.githubusercontent.com/ZenitH-AT/nvidia-update/main/LICENSE
@@ -32,7 +32,7 @@ if (-not (Get-NetRoute | Where-Object DestinationPrefix -eq "0.0.0.0/0" | Get-Ne
 	Write-ExitError "No internet connection. After resolving connectivity issues, please try running this script again."
 }
 
-## Register scheduled task
+## Download latest release files
 $taskDir = "$($env:USERPROFILE)\nvidia-update"
 $taskPath = "$($taskDir)\$($defaultScriptFileName)"
 
@@ -40,16 +40,18 @@ if (-not (Test-Path $taskDir)) {
 	New-Item -Path $taskDir -ItemType "directory" | Out-Null
 }
 
-# Get latest release version
 try {
 	$latestReleaseUrl = [System.Net.WebRequest]::Create("https://github.com/ZenitH-AT/nvidia-update/releases/latest").GetResponse().ResponseUri.OriginalString
-	$latestReleaseVersion = $latestReleaseUrl.Split("/")[-1]
+
+	Invoke-WebRequest -Uri "$($latestReleaseUrl.Replace("tag", "download"))/$($defaultScriptFileName)" -OutFile $taskPath
+	Invoke-WebRequest -Uri "$($latestReleaseUrl.Replace("tag", "download"))/$($defaultConfigFileName)" -OutFile "$($taskDir)\$($defaultConfigFileName)"
 }
 catch {
-	Write-ExitError "Unable to determine latest script version. Please try running this script again."
+	Write-ExitError "Downloading script files failed. Please try running this script again."
 }
 
-$taskName = "nvidia-update $($latestReleaseVersion)"
+## Register scheduled task
+$taskName = "nvidia-update $($latestReleaseUrl.Split("/")[-1])"
 $description = "NVIDIA Driver Update"
 $scheduleDay = "Sunday"
 $scheduleTime = "12pm"
@@ -58,43 +60,18 @@ $action = New-ScheduledTaskAction -Execute $powershellExe -Argument "-File `"$($
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -RunOnlyIfIdle -IdleDuration 00:10:00 -IdleWaitTimeout 04:00:00
 $trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $scheduleDay -At $scheduleTime
 
-# Register task if it doesn't already exist or if it references a different script version (and delete outdated tasks)
-$registerTask = $true
-$existingTasks = Get-ScheduledTask | Where-Object TaskName -match "^nvidia-update."
-
-foreach ($existingTask in $existingTasks) {
-	$registerTask = $false
-
-	if ($existingTask.TaskName -notlike "*$($latestReleaseVersion)") {
-		Unregister-ScheduledTask -TaskName $existingTask.TaskName -Confirm:$false
-
-		$registerTask = $true
-	}
+# Unregister any existing nvidia-update tasks
+foreach ($existingTask in Get-ScheduledTask | Where-Object TaskName -match "^nvidia-update.") {
+	Unregister-ScheduledTask -TaskName $existingTask.TaskName -Confirm:$false
 }
 
-if ($registerTask) {
-	# Download latest release files
-	try {
-		Invoke-WebRequest -Uri "$($latestReleaseUrl.Replace("tag", "download"))/$($defaultScriptFileName)" -OutFile $taskPath
-		Invoke-WebRequest -Uri "$($latestReleaseUrl.Replace("tag", "download"))/$($defaultConfigFileName)" -OutFile "$($taskDir)\$($defaultConfigFileName)"
-	}
-	catch {
-		Write-ExitError "Downloading script files failed. Please try running this script again."
-	}
+Write-Host "Downloaded the latest script to `"$($taskPath)`".`n"
+Register-ScheduledTask -TaskName $taskName -Action $action -Settings $settings -Trigger $trigger -Description $description | Out-Null
+Write-Host -ForegroundColor Green "Scheduled task registered."
 
-	Write-Host "Downloaded the latest script to `"$($taskPath)`".`n"
-	Register-ScheduledTask -TaskName $taskName -Action $action -Settings $settings -Trigger $trigger -Description $description | Out-Null
-	Write-Host -ForegroundColor Green "Scheduled task registered."
-}
-else {
-	Write-Host -ForegroundColor Gray "Scheduled task for the latest script version is already registered."
-}
-
+## Offer to run nvidia-update
 $decision = $Host.UI.PromptForChoice("", "`nWould you like to run nvidia-update?", ("&Yes", "&No"), 0)
 
 if ($decision -eq 0) {
-	Start-Process -FilePath "powershell" -ArgumentList "-File `"$($taskPath)`""
+	Start-Process -FilePath $powershellExe -ArgumentList "-File `"$($taskPath)`""
 }
-
-Write-Host "`nExiting script in 5 seconds..."
-Start-Sleep -Milliseconds 5000
